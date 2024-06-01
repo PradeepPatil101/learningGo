d4f7b255dd0f5c3b4eeee08448b425a5079bdab7
Pradeep Patil <pradeeppatil@Pradeeps-Air.home>
2024-06-01 14:47:50 -0500

    finished mocking

--- dependencyInjection/di.go
+++ dependencyInjection/di.go
@@ -5,13 +5,12 @@ package dependencyInjection
 import (
 	"fmt"
 	"io"
-	"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
-func main() {
-	Greet(os.Stdout, "Elodie")
-}
+// func main() {
+// 	Greet(os.Stdout, "Elodie")
+// }
