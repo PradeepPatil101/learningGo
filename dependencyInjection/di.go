43665981b6f21b21ee18a5881d84f7c9af6c8ba1
Pradeep Patil <pradeeppatil@Pradeeps-Air.home>
2024-06-01 13:48:16 -0500

    Completed dependency injection

---
+++ dependencyInjection/di.go
@@ -0,0 +1,17 @@
+// The main purpose of this is to show that Greet can be used in application as well as for testing
+
+package dependencyInjection
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+}
